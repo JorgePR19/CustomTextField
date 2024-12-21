847a057569a45b499a17e9e66be3fa657ab0664e
jorgeperez <jorgearmandop.rivera@gmail.com>
2024-12-21 17:09:31 -0600

    config jitpack

---
+++ customtextfield/src/main/java/com/example/customtextfield/themes/dimens/DimensSp.kt
@@ -0,0 +1,9 @@
+package com.example.customtextfield.themes.dimens
+/*
+import androidx.compose.ui.unit.sp
+
+object DimensSp {
+    val Sp14 = 14.sp
+    val Sp16 = 16.sp
+    val Sp24 = 24.sp
+}*/
---
+++ customtextfield/src/main/java/com/example/customtextfield/themes/styles/Fonts.kt
@@ -0,0 +1,49 @@
+package com.example.customtextfield.themes.styles
+/*
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.customtextfield.R
+import com.example.customtextfield.themes.dimens.DimensSp.Sp14
+import com.example.customtextfield.themes.dimens.DimensSp.Sp16
+import com.example.customtextfield.themes.dimens.DimensSp.Sp24
+
+object Fonts {
+    private val robotoFontFamily = FontFamily(
+        Font(R.font.roboto_regular, FontWeight.Normal),
+        Font(R.font.roboto_bold, FontWeight.Bold),
+        Font(R.font.roboto_light, FontWeight.Light),
+        Font(R.font.roboto_medium, FontWeight.Medium)
+    )
+
+
+    var roboto16Medium = TextStyle(
+        fontSize = Sp16,
+        lineHeight = Sp24,
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Medium,
+    )
+    var roboto14Medium = TextStyle(
+        fontSize = Sp14,
+        lineHeight = Sp16,
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Medium,
+        platformStyle = PlatformTextStyle(includeFontPadding = false)
+    )
+    var roboto16Regular = TextStyle(
+        fontSize = Sp16,
+        fontWeight = FontWeight.Normal,
+        fontFamily = robotoFontFamily,
+        lineHeight = Sp24,
+        platformStyle = PlatformTextStyle(includeFontPadding = false)
+    )
+    var roboto14Regular = TextStyle(
+        fontSize = Sp14,
+        fontWeight = FontWeight.Normal,
+        fontFamily = robotoFontFamily,
+        lineHeight = Sp16
+    )
+
+}*/
